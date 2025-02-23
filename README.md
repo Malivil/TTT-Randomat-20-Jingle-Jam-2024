c749a64b4ae562bc155c4523955fd61170f746a6
Malivil <mlebson@outlook.com>
2025-02-22 23:04:21 -0500

    Added JingleJam2024

--- README.md
+++ README.md
@@ -3,16 +3,23 @@ A pack of [Randomat 2.0](https://github.com/Malivil/TTT-Randomat-20) events crea
 
 # Events
 
-## Event
-_Contributed By_: ABC\
-Does a thing
+## Jingle Jam 2024
+_Contributed By_: Malivil\
+Assigns a random instrument (Electric Guitar, Bass Guitar, Acoustic Guitar, Drums, or Piano) to each player.\
+When that player fires their weapon, a tone for their assigned instrument is played based on the pitch (up and down angle) their weapon is aimed at.
 \
 \
 **ConVars**
 \
-_ttt_randomat_event_ - Default: 1 - Whether this event is enabled.\
-_ttt_randomat_event_min_players_ - Default: 0 - The minimum number of players required for this event to start.\
-_ttt_randomat_event_weight_ - Default: -1 - The weight this event should use during the randomized event selection process.
+_ttt_randomat_jinglejam2024_ - Default: 1 - Whether this event is enabled.\
+_ttt_randomat_jinglejam2024_min_players_ - Default: 0 - The minimum number of players required for this event to start.\
+_ttt_randomat_jinglejam2024_weight_ - Default: -1 - The weight this event should use during the randomized event selection process.
 
 # Special Thanks
-- TODO
+- [SampleFocus.com](https://samplefocus.com) for the Acoustic and Base Guitar sounds used in the "Jingle Jam 2024" event
+  - "Guitar" and "Bass Guitar" by [SimonProTV250](https://samplefocus.com/users/simonprotv250)
+  - "Nylon Guitar" by [Buena Morena](https://samplefocus.com/users/buena-morena)
+- [GFXSounds.com](https://gfxsounds.com) for the Drum and Electric Guitar sounds used in the "Jingle Jam 2024" event
+  - Both sets of sounds by [Samuel Oddamo](https://gfxsounds.com/blog/samuel-oddamo/)
+- [FreeSound.org](https://freesound.org) for the Piano sounds used in the "Jingle Jam 2024" event
+  - Sounds by [pinkyfinger](https://freesound.org/people/pinkyfinger/)
---
+++ lua/randomat2/events/jinglejam2024.lua
@@ -0,0 +1,129 @@
+local math = math
+local string = string
+local util = util
+
+local StringFind = string.find
+local StringReplace = string.Replace
+local MathCeil = math.ceil
+
+local EVENT = {}
+
+EVENT.Title = "Jingle Jam 2024"
+EVENT.Description = "You've joined a jam band! Each player was assigned a random instrument"
+EVENT.id = "jinglejam2024"
+EVENT.Type = EVENT_TYPE_GUNSOUNDS
+EVENT.Categories = {"fun", "smallimpact"}
+
+-- Instruments toward the start of this list will be used more often than the ones at the end
+local sounds = {
+    Guitar = {
+        "GuitarC.mp3", "GuitarD.mp3", "GuitarE.mp3", "GuitarF.mp3", "GuitarG.mp3", "GuitarA.mp3",
+        "GuitarHeavyELow.mp3", "GuitarHeavyA.mp3", "GuitarHeavyG.mp3", "GuitarHeavyEHigh.mp3", "GuitarHeavyPluck.mp3", "GuitarHeavyPluck2.mp3"
+    },
+    ["Bass Guitar"] = {
+        "BassC.mp3", "BassD.mp3", "BassE.mp3", "BassF.mp3", "BassG.mp3", "BassA.mp3",
+        "ElectricBassC.mp3", "ElectricBassD.mp3", "ElectricBassE.mp3", "ElectricBassF.mp3", "ElectricBassG.mp3", "ElectricBassB.mp3"
+    },
+    Drums = {
+        "Kick.mp3", "Kick2.mp3", "ConcertTom.mp3", "FloorTom.mp3", "Tom.mp3", "Snare.mp3",
+        "Snare2.mp3", "HiHat.mp3", "CymbalCrash.mp3", "CymbalShort.mp3", "CymbalMedium.mp3", "CymbalLong.mp3"
+    },
+    ["Acoustic Guitar"] = {
+        "AcousticC.mp3", "AcousticD.mp3", "AcousticE.mp3", "AcousticF.mp3", "AcousticG.mp3", "AcousticA.mp3",
+        "NylonBMinor.mp3", "NylonDflatMinor.mp3", "NylonEMajor.mp3", "NylonFsharpMaj9.mp3", "NylonFsharpMinor.mp3", "NylonAflatMajor.mp3"
+    },
+    Piano = {
+        "PianoA.mp3", "PianoGSharp.mp3", "PianoBFlat.mp3", "PianoB.mp3", "PianoC.mp3", "PianoCSharp.mp3",
+        "PianoD.mp3", "PianoEFlat.mp3", "PianoE.mp3", "PianoF.mp3", "PianoFSharp.mp3", "PianoG.mp3"
+    }
+}
+local soundsFolder = {}
+local ignorePaths = {
+    ".*weapons/.*explode.*%..*", ".*weapons/.*beep.*%..*",
+    ".*weapons/.*out%..*", ".*weapons/.*in%..*",
+    ".*weapons/.*reload.*%..*", ".*weapons/.*deploy.*%..*",
+    ".*weapons/.*insertshell.*%..*", ".*weapons/.*selectorswitch.*%..*",
+    ".*weapons/.*fetchmag.*%..*", ".*weapons/.*magslap.*%..*",
+    ".*weapons/.*beltjingle.*%..*", ".*weapons/.*beltalign.*%..*",
+    ".*weapons/.*lidopen.*%..*", ".*weapons/.*lidclose.*%..*",
+    ".*weapons/.*coverup.*%..*", ".*weapons/.*coverdown.*%..*",
+    ".*weapons/.*rattle.*%..*", ".*weapons/.*slidepull.*%..*",
+    ".*weapons/.*slideback.*%..*", ".*weapons/.*sliderelease.*%..*",
+    ".*weapons/.*bolt.*%..*", ".*weapons/.*shotgun_shell.*%..*",
+    ".*weapons/.*empty.*%..*", ".*weapons/.*zoom.*%..*"
+}
+
+
+local function GetWeaponSound(ply, pitch)
+    local instrument = ply:GetNWString("RdmtJingleJam2024Instrument", "")
+    if #instrument == 0 then return end
+
+    local index = MathCeil(pitch / 15)
+    return soundsFolder[instrument] ..  "/" .. sounds[instrument][index]
+end
+
+local function ShouldIgnoreSound(soundName)
+    -- If this isn't a weapon sound, ignore it
+    if not StringFind(soundName, "weapon") then return true end
+
+    -- If any of these patterns match the sound name, ignore it
+    for _, pattern in ipairs(ignorePaths) do
+        if string.find(soundName, pattern) then return true end
+    end
+
+    return false
+end
+
+function EVENT:Initialize()
+    local instruments = table.GetKeys(sounds)
+    for _, i in pairs(instruments) do
+        soundsFolder[i] = StringReplace(i, " ", "")
+    end
+
+    for _, i in pairs(instruments) do
+        local instrumentFolder = soundsFolder[i]
+        for _, s in ipairs(sounds[i]) do
+            util.PrecacheSound(instrumentFolder .. "/" .. s)
+        end
+    end
+end
+
+function EVENT:Begin()
+    local currentInstrument = 1
+    local instruments = table.GetKeys(sounds)
+    local maxInstruments = table.Count(instruments)
+    for _, ply in ipairs(self:GetAlivePlayers(true)) do
+        local instrument = instruments[currentInstrument]
+        ply:SetNWString("RdmtJingleJam2024Instrument", instrument)
+
+        -- Tell the player their instrument
+        timer.Simple(0.1, function()
+            Randomat:PrintMessage(ply, MSG_PRINTBOTH, "You have the " .. instrument)
+        end)
+
+        currentInstrument = currentInstrument + 1
+        -- Loop through the instruments repeatedly
+        if currentInstrument > maxInstruments then
+            currentInstrument = 1
+        end
+    end
+
+    self:AddHook("EntityEmitSound", function(data)
+        if ShouldIgnoreSound(data.SoundName:lower()) then return end
+        if not IsPlayer(data.Entity) then return end
+
+        local aim = data.Entity:EyeAngles()
+        aim:Normalize()
+
+        -- Negate the pitch because in GMod looking down is positive and looking up is negative
+        local pitch = -aim.pitch
+        -- Normalize the pitch to within 0-180 for easier indexing
+        pitch = pitch + 90
+
+        local chosen_sound = GetWeaponSound(data.Entity, pitch)
+        data.SoundName = chosen_sound
+        return true
+    end)
+end
+
+Randomat:register(EVENT)
