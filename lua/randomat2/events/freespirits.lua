f3ba9268ed66545ec40d0f5dc1181a54d1ee67d4
Malivil <mlebson@outlook.com>
2025-03-22 18:42:14 -0400

    Changed freespirits to make already-dead players become ghosts immediately

--- lua/randomat2/events/freespirits.lua
+++ lua/randomat2/events/freespirits.lua
@@ -5,18 +5,26 @@ EVENT.Description = "Dead players become empowered spirits capable of talking wi
 EVENT.id = "freespirits"
 EVENT.Categories = {"deathtrigger", "spectator", "largeimpact", "eventtrigger"}
 
+local function GhostPlayer(ply)
+    ply:QueueMessage(MSG_PRINTBOTH, "'"  ..  Randomat:GetEventTitle(EVENT) .. "' has granted you the ability to talk in chat and use abilities!")
+    ply:QueueMessage(MSG_PRINTBOTH, "Select your abilit" .. (cvars.Number("ttt_ghostwhisperer_max_abilities", -1) == 1 and "y" or "ies") .. " in the shop")
+    ply:SetProperty("TTTIsGhosting", true, ply)
+end
+
 function EVENT:Begin()
     -- If only ghost whisperers can normally read ghost chat, start "deadchat" so everyone can read it instead (if it exists)
     if cvars.Bool("ttt_ghostwhisperer_limited_chat", false) and Randomat.Events["deadchat"] then
         Randomat:SilentTriggerEvent("deadchat", self.owner)
     end
 
+    -- Non-spectator dead players become ghosts immediately
+    for _, p in self:GetDeadPlayers(false, false) do
+        GhostPlayer(p)
+    end
+
     self:AddHook("PostPlayerDeath", function(ply)
         if not IsPlayer(ply) then return end
-
-        ply:QueueMessage(MSG_PRINTBOTH, "'"  ..  Randomat:GetEventTitle(self) .. "' has granted you the ability to talk in chat and use abilities!")
-        ply:QueueMessage(MSG_PRINTBOTH, "Select your abilit" .. (cvars.Number("ttt_ghostwhisperer_max_abilities", -1) == 1 and "y" or "ies") .. " in the shop")
-        ply:SetProperty("TTTIsGhosting", true, ply)
+        GhostPlayer(ply)
     end)
 end
 
